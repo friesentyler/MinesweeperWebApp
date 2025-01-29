25b98c5fd83e0c5564c228a6172e8b0d8f13e5b3
tylerfriesen <tfriesen2@my.gcu.edu>
2025-01-29 15:48:09 -0800

    added new controller and models

---
+++ MinesweeperWebApp/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+
+namespace MinesweeperWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserManager users;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        public IActionResult ProcessRegister(RegisterViewModel registerViewModel)
+        {
+            UserModel user = new UserModel();
+            user.Username = registerViewModel.Username;
+            user.SetPassword(registerViewModel.Password);
+            user.Groups = "";
+            foreach (var group in registerViewModel.Groups)
+            {
+                if (group.IsSelected)
+                {
+                    user.Groups += group.GroupName + ",";
+                }
+            }
+            user.Groups = user.Groups.TrimEnd(',');
+            users.AddUser(user);
+
+            return View("Index");
+        }
+    }
+}
