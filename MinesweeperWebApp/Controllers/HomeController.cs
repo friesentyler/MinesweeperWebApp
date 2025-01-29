179404e61e06ab47084674ce891043d08831dba6
tylerfriesen <tfriesen2@my.gcu.edu>
2025-01-29 15:05:56 -0800

    testing

--- MinesweeperWebApp/Controllers/HomeController.cs
+++ MinesweeperWebApp/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace MinesweeperWebApp.Controllers
             return View();
         }
 
+        public IActionResult DELETECONTROLLER()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
