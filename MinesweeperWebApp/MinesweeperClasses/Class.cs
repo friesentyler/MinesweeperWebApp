72ea0fb10115cea11fb9939856c50633a11a3393
tylerfriesen <tfriesen2@my.gcu.edu>
2025-02-07 17:22:22 -0800

    fixed namespace

--- MinesweeperWebApp/MinesweeperClasses/Class.cs
+++ MinesweeperWebApp/MinesweeperClasses/Class.cs
@@ -9,7 +9,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MineSweeperWebApp.
+namespace MineSweeperWebApp.MinesweeperClasses
 {
 public class Board
 {
