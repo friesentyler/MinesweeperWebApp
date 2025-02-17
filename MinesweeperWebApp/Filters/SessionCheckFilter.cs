f716c71b27011bc31cb5855a71871217472e0a33
Kolter Leisy <KLeisy@my.gcu.edu>
2025-02-16 19:17:13 -0700

    added session checking to ensure same user

--- MinesweeperWebApp/Filters/SessionCheckFilter.cs
+++ MinesweeperWebApp/Filters/SessionCheckFilter.cs
@@ -7,9 +7,18 @@ namespace MinesweeperWebApp.Filters
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
+            string GameStarter = context.HttpContext.Session.GetString("GameStarter");
+            string User = context.HttpContext.Session.GetString("User");
+            Console.WriteLine(GameStarter);
+            
             if (context.HttpContext.Session.GetString("User") == null)
             {
                 context.Result = new RedirectResult("/User/Index");
+            }
+            else if (User != GameStarter)
+            {
+                context.Result = new RedirectResult("/Game/AccessDenied"); // Redirect unauthorized users
+                return;
             }
         }
     }
