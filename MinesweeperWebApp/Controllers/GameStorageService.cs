88e6a04b32db2d26873e9e3afb90523e1d6713f1
tylerfriesen <tfriesen2@my.gcu.edu>
2025-03-28 08:17:35 -0700

    Added Game Storage Service REST API

---
+++ MinesweeperWebApp/Controllers/GameStorageService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinesweeperWebApp.Controllers
+{
+    [Route("api")]
+    public class GameStorageService : Controller
+    {
+        public JsonResult showSavedGames(int? id)
+        {
+            return Json("showed");
+        }
+
+        [HttpPost]
+        public JsonResult deleteOneGame(int id)
+        {
+            return Json("deleted");
+        }
+    }
+}
